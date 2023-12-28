94d2ea8da8ac461a8072d9c200eacbb354249021
Daria Malmgren <daria.malmgren@gmail.com>
2023-12-28 14:54:03 +0100

    3rd task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,17 @@
 function camelize(str) {
-  // ваш код...
+  return str
+    // Split the string into an array of words wherever a hyphen occurs
+    .split('-')
+    // Iterate through each word in the array
+    .map((word, index) => {
+      if (index === 0) {
+        // The first word remains untouched
+        return word;
+      } else {
+        // Subsequent words are written with the capital letter
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      }
+    })
+    // Bring the modified words back together into a single string without any separators
+    .join('');
 }
