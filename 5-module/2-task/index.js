91c22c3d322acc3a7aef51dd8dacdf3c911625ea
Daria Malmgren <daria.malmgren@gmail.com>
2024-01-15 12:29:39 +0100

    2nd task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
-  // ваш код...
+  let toggleButton = document.querySelector('.toggle-text-button');
+  let textDiv = document.getElementById('text');
+  if (toggleButton && textDiv) {
+    toggleButton.addEventListener('click', function () {
+      if (textDiv.hidden) {
+        textDiv.hidden = false;
+      } else {
+        textDiv.hidden = true;
+      }
+    });
+  }
 }
